05fe3027dcd15001913031dc270718ee74bc5dc3
mdhossain07 <mrabir8097@gmail.com>
2024-01-14 19:04:56 +0600

    task modal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import TaskBoard from "./Components/Task/TaskBoard";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <div className="flex flex-col justify-center items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
