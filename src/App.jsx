c31ed5f1e16871c23954a15bfa59a5b4df42351e
mdhossain07 <mrabir8097@gmail.com>
2024-01-14 11:23:49 +0600

    hero & footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Footer />
     </>
   );
 }
